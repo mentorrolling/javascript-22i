2b7cda1c0701c770d59c6fc00b18f4a2267a296b
Pablo Marino <pmarino2013@gmail.com>
2021-12-06 21:44:49 -0300

    Clase y ejercicios de POO

---
+++ fundamentos/13-POO.js
@@ -0,0 +1,71 @@
+class Usuario {
+  constructor(nombre, username, password) {
+    this.nombre = nombre;
+    this.username = username;
+    this.password = password;
+  }
+  presentacion() {
+    console.log("======Tarjeta de presentación======");
+    console.log(`Nombre: ${this.nombre}`);
+    console.log(`UserName: ${this.username}`);
+  }
+}
+
+const user1 = new Usuario("Pablo", "pmarino", "123456");
+const user2 = new Usuario("Gonzalo", "gwinner", "ABC123");
+
+class Administrador extends Usuario {
+  constructor(nombre, username, password, puesto, activo = true) {
+    super(nombre, username, password);
+    this.puesto = puesto;
+    this.activo = activo;
+  }
+
+  cambiarActivo() {
+    this.activo = !this.activo;
+  }
+
+  saludarAdmin() {
+    super.presentacion();
+    console.log(`Puesto: ${this.puesto}`);
+  }
+}
+
+const admin1 = new Administrador(
+  "Miqueas",
+  "miqueas2021",
+  "12345678",
+  "Gerente regional"
+);
+
+/*
+    
+    Escribe un programa que cree un objeto "cuenta" con las siguientes propiedades:
+    Una propiedad titular.
+    Una propiedad saldo, teniendo como valor inicial 0.
+    Un método ingresar() que permita añadir dinero a la cuenta, pasando la cantidad como parámetro
+    Un método extraer() que permita retirar la cantidad pasada como parámetro.
+    Un método informar() que retorne la información del estado de la cuenta. 
+    
+    Utiliza este objeto para mostrar la descripción, ingresar y extraer dinero y volver a mostrar la descripción del estado de la cuenta.
+    Rectángulos
+    
+    */
+
+class Cuenta {
+  constructor(titular, saldo = 0) {
+    this.titular = titular;
+    this.saldo = saldo;
+  }
+  ingresar(valor) {
+    this.saldo += valor;
+  }
+  extraer(valor) {
+    this.saldo -= valor;
+  }
+  informar() {
+    console.log("====Datos de la cuenta====");
+    console.log(`Titular: ${this.titular}`);
+    console.log(`Saldo: ${this.saldo}`);
+  }
+}
